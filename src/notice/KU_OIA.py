2d37ae44fa929a0942d0345954d7a51a00cef77e
ONEASH <oneash0824@naver.com>
2023-02-19 19:39:05 +0900

    KU_OIA

--- src/notice/KU_OIA.py
+++ src/notice/KU_OIA.py
@@ -11,7 +11,7 @@ from MyDB import MyDB
 from selenium.webdriver.common.by import By
 
 # 사이트
-site_name = 'KStartup'
+site_name = '건국대국제교류협력팀'
 
 # 텔레그램 봇
 bot = MyBot(KEY.TELEGRAM_TOKEN.value)
@@ -24,22 +24,24 @@ db.connect_db_SUBSCRIPTION()
 # SOUP
 url = db.get_url(site_name) # 공지 url
 try:
-    soup = create_soup_selenium(url, KEY.USER_AGENT.value)
-    notice_list = soup.select("li.notice")
+    browser_action = 'browser.switch_to.frame("iframe_main")'
+    soup = create_soup_selenium(url, KEY.USER_AGENT.value, browser_action=browser_action)
+
+    notice_list = soup.select("tbody tr")
     if notice_list == []: # 무슨 이유에서든, 제대로 크롤링이 안됐을 때
         raise Exception('notice_list가 비었습니다')
 
     for notice in notice_list:
         href = notice.a['href']
-        id = href.replace("javascript:go_view(", "").replace(");", "")
+        id = href.split('&')[2].replace('id=', '')
 
         check = db.check_SITELOG(site_name, id) # DB에 해당 id 있나 확인
         if check == 1: # 있을 경우
             break
         else: # 없을 경우
-            title = notice.a.p.text
+            title = notice.a.text
             title = format_string(title)
-            link = f"https://www.k-startup.go.kr/web/contents/bizpbanc-ongoing.do?schM=view&pbancSn={id}&page=1&schStr=regist&pbancEndYn=N"
+            link = "http://www.konkuk.ac.kr/do/MessageBoard/" + href
             db.save_SITELOG(site_name, id, title, link) # db SITELOG에 게시물 기록 저장
 
             # 텔레그램으로 구독자들에게 공지
