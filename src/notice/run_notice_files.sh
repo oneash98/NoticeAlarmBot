42bf0d03f7aacf489f4b808e769eb808767d6913
ONEASH <oneash0824@naver.com>
2023-02-19 19:19:23 +0900

    sh

--- src/notice/run_notice_files.sh
+++ src/notice/run_notice_files.sh
@@ -2,4 +2,5 @@
 
 cd /home/oneash082498/NoticeAlarmBot/src/notice
 
+
 python3 Yonsei_Nursing.py ; python3 Yonsei_MSE.py ; python3 Yonsei_Engineering.py ; python3 KNU.py ; python3 KNU_electric.py ; python3 KNU_SEE_employment.py ; python3 K-Startup.py 
