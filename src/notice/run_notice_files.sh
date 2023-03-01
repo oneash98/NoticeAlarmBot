fb144e4362663c0413b0f64f8c4ada22e4a1bf62
ONEASH <oneash0824@naver.com>
2023-03-01 22:33:29 +0900

    KNU job

--- src/notice/run_notice_files.sh
+++ src/notice/run_notice_files.sh
@@ -3,4 +3,4 @@
 cd /home/oneash082498/NoticeAlarmBot/src/notice
 
 
-python3 Yonsei_Nursing.py ; python3 Yonsei_MSE.py ; python3 Yonsei_Engineering.py ; python3 KNU.py ; python3 KNU_electric.py ; python3 KNU_SEE_employment.py ; python3 K-Startup.py ; python3 KU_OIA.py ; python3 KNU_global.py ; python3 KNU_bachelor.py
+python3 Yonsei_Nursing.py ; python3 Yonsei_MSE.py ; python3 Yonsei_Engineering.py ; python3 KNU.py ; python3 KNU_electric.py ; python3 KNU_SEE_employment.py ; python3 K-Startup.py ; python3 KU_OIA.py ; python3 KNU_global.py ; python3 KNU_bachelor.py ; python3 KNU_job.py
